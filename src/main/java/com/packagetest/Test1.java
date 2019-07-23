6d3ddf8488235ca01ea443faa8c497e61066948f
Arun Kumar <arun.kumar@lfg.com>
2019-07-23 06:05:57 -0400

    Added new print line

--- src/main/java/com/packagetest/Test1.java
+++ src/main/java/com/packagetest/Test1.java
@@ -3,6 +3,8 @@ package com.packagetest;
 public class Test1 {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Devops 301");
 		System.out.println("Added line 1 in Test1");
 
 	}
